f83e98392084f44a84ed365ea9404a2c368f6446
Jetix24 <josepabloaguirre02@gmail.com>
2023-04-24 20:15:04 -0600

    ADD: Variables

---
+++ variables.py
@@ -0,0 +1,12 @@
+
+
+name = "Jose"
+last_name = "Aguirre"
+age = 21
+
+list_shopping = {"Huevos","Leche","Harina"}
+
+print(f"{name} {last_name} {age}")
+
+print(name + " " + last_name + " "+ age)
+print(list_shopping)
