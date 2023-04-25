4a973ce0d787450bba035cb85d233b0c05d7e4af
Jetix24 <josepabloaguirre02@gmail.com>
2023-04-24 20:23:36 -0600

    ADD: Operators

---
+++ operators.py
@@ -0,0 +1,24 @@
+
+addition = 20 +5 #Esto dara como rsultado 15
+substraccion = 10 -5
+multiplicacion = 10*5
+
+
+
+### Comparacion de operadores
+age = 10 == 5
+age = 10 != 5
+age = 10 > 5
+
+### Operadores logicos
+
+age_boy = 10
+age_girl = 14
+
+age_boy > 8 and age_girl > 8 #True
+
+### Operadores de asignacion
+age = 10 #10
+age += 5 #15
+age -= 5 #10
+age *= 5 #50
