1e5d531f6bdc875545d7e8f3764074ae5b1b0e66
Jetix24 <josepabloaguirre02@gmail.com>
2023-04-26 19:34:54 -0600

    UPD: Convert interger to string

--- variables.py
+++ variables.py
@@ -8,5 +8,5 @@ list_shopping = {"Huevos","Leche","Harina"}
 
 print(f"{name} {last_name} {age}")
 
-print(name + " " + last_name + " "+ age)
+print(name + " " + last_name + " "+ str(age))
 print(list_shopping)
